e23f745f09981751967f80ab6252f25dafb0b193
tranjamin <55745642+tranjamin@users.noreply.github.com>
2019-11-09 07:39:28 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -293,7 +293,7 @@ function ajaxSearch (url, search) {
                 includes_tag = true;
                 break;
             }
-            else if (InnerHTML[index+i] == "<" && InnerHTML[index+i+1] == "/") {
+            else if (InnerHTML[index+i] == "<" && (/p/.test(InnerHTML[index+i+1])||(InnerHTML[index+i+1] == "/"))) {
                 if (InnerHTML.substring(index+i,index+i+9) == "</script>") {
                     includes_tag = true;
                     break;
