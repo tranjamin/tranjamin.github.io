664869ccb6b90f82ff32526a5e35570a50b88026
tranjamin <55745642+tranjamin@users.noreply.github.com>
2020-07-10 23:26:39 +1000

    statement

---
+++ chess/statement.js
@@ -0,0 +1,124 @@
+/*
+This Version Includes:
+Basic piece movement
+Basic chat
+En passant
+Castling
+Promotion
+
+This Version does not Include:
+Check / Checkmate
+Underpromotion
+Undo
+Flip Board
+
+*/
+
+// socket
+
+
+function $(id) { return document.getElementById(id); }
+
+
+// sockets
+  
+
+var username = "anon";
+var user_id = "";
+var successful_email;
+
+
+var options = $('options');
+
+window.addEventListener('resize', e => {
+    update_graphics();
+})
+
+if (getCookie('username') || sessionStorage.getItem('username')) {
+        $('nav').getElementsByTagName('button')[0].innerHTML = "Welcome, ";
+    $('nav').getElementsByTagName('button')[0].innerHTML += sessionStorage.getItem('username') ? sessionStorage.getItem('username') : getCookie('username');
+$('nav').getElementsByTagName('button')[0].innerHTML += "<br>Logout";
+}
+
+
+function update_graphics() {
+    var overlay = $('overlay');
+    overlay.style.height = window.innerHeight * 0.95 + 'px';
+    overlay.style.width = overlay.style.height;
+    overlay.style.left = (window.innerWidth - overlay.style.width.slice(0,-2)) / 2 + "px";
+    overlay.style.top = window.innerHeight * 0.02 + "px";
+
+    //nav
+    var nav = $('nav');
+    nav.style.height = window.innerHeight + "px";
+    nav.style.width = screen.width * 0.125 + "px";
+    nav.style.top = 0;
+    nav.style.left = 0;
+}
+
+update_graphics();
+
+$('nav').getElementsByTagName('li')[0].addEventListener('click', e => {
+    if ($('nav').getElementsByTagName('button')[0].innerHTML == "<a href='signup.html'>Login/Signup</a>") {window.location.assign('signup.html')}
+    else {
+        sessionStorage.clear();
+        deleteAllCookies();
+        location.reload();}
+});
+$('nav').getElementsByTagName('li')[1].addEventListener('click', e => {
+        location.assign('signup.html');
+});
+$('nav').getElementsByTagName('li')[2].addEventListener('click', e => {
+    location.assign('create.html');
+});
+$('nav').getElementsByTagName('li')[3].addEventListener('click', e => {
+    location.assign('load.html');
+});
+$('nav').getElementsByTagName('li')[4].addEventListener('click', e => {
+    location.assign('about.html');
+});
+$('nav').getElementsByTagName('li')[5].addEventListener('click', e => {
+    location.assign('statement.html');
+});
+
+function sendEmail (email_address, subject, body) {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "tranjaminchess.noreply@gmail.com",
+        Password: 'Tranjaminchess',
+        To: email_address,
+        From: "tranjaminchess.noreply@gmail.com",
+        Subject: subject,
+        Body: body
+    }).then(error => {successful_email = error})
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+
+function deleteAllCookies() {
+    document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+}
